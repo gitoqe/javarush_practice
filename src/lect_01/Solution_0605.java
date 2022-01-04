280ab87af11848beae72776dc9f2f9d547f6e2be
Maxim <creativesun@yandex.ru>
2022-01-04 23:54:17 +0300

    add lect_01 task_0605

---
+++ src/lect_01/Solution_0605.java
@@ -0,0 +1,22 @@
+package lect_01;
+
+public class Solution_0605 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+    // Реализовать метод reverseArray(). Он должен менять порядок элементов массива на обратный.
+    // Метод должен работать только с массивами целочисленных значений (int[]).
+    public static void reverseArray(int[] array) {
+
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
